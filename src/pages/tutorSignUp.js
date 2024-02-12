c58830ac3d2b984c1fc64fddb7532b4cb3e0e295
unknown <yessine.chalouati@gmail.com>
2024-02-12 21:07:09 +0100

    n

--- src/pages/tutorSignUp.js
+++ src/pages/tutorSignUp.js
@@ -1,11 +1,9 @@
 
-function TutorSignUp() {
+export default function TutorSignUp() {
     return (
         <div className="bg-backg relative w-screen h-screen flex justify-center items-center">
-            <div className="h-full w-[20%]"></div>
-            <img src="teach.jgp" alt="tutorbg" className="h-full w-[70%]"></img>
+            <div className="h-full w-[20%]"> hello</div>
+            <img src="teach.jpg" alt="tutorbg" className="h-full w-[70%]"></img>
         </div>
     );
 }
-
-export default TutorSignUp;
