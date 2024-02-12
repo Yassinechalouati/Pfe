74abed2673c2214e15b80a7c95629ca3bc6631d1
unknown <yessine.chalouati@gmail.com>
2024-02-12 21:17:30 +0100

    setup tutorSignUp

--- src/pages/tutorSignUp.js
+++ src/pages/tutorSignUp.js
@@ -2,8 +2,10 @@
 export default function TutorSignUp() {
     return (
         <div className="bg-backg relative w-screen h-screen flex justify-center items-center">
-            <div className="h-full w-[20%]"> hello</div>
-            <img src="teach.jpg" alt="tutorbg" className="h-full w-[70%]"></img>
+            <div className="h-full w-[30%]">
+                
+            </div>
+            <img src="/teach.jpg" alt="tutorbg" className="h-full w-[70%] object-cover"></img>
         </div>
     );
 }
